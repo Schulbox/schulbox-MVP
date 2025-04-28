cfc58d4add441f14e5575da2f4c5a21ea87b02dd
Schulbox <office@schulbox.at>
2025-04-28 20:56:17 +0200

    Prompt für Manus formuliert und vorbereitet

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,10 @@
-/** @type {import('@remix-run/dev').AppConfig} */
 export default {
-    future: {
-      v2_routeConvention: true,
-    },
-  };
-  
+  ignoredRouteFiles: ["**/.*"],
+  future: {
+    v2_routeConvention: true,
+    v2_errorBoundary: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
+  },
+  serverModuleFormat: "esm",
+};
