44f38757e554b33ae8ec5e27198afa843b592ed0
Schulbox <office@schulbox.at>
2025-04-28 20:53:20 +0200

    Prompt für Manus formuliert und vorbereitet

--- app/routes.ts
+++
@@ -1,9 +0,0 @@
-import { flatRoutes } from "remix-flat-routes";
-
-export default function routes(defineRoutes: any) {
-  return flatRoutes("routes", defineRoutes, {
-    ignoredRouteFiles: ["**/.*"],
-    // Verwende einen anderen Charakter als '+' für Vercel-Kompatibilität
-    nestedDirectoryChar: '_',
-  });
-}
